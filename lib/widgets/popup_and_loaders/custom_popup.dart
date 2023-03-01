5fc9923bda2857c60676dd0726e75d46a79718d7
Greejith <app.greejith@inmotionmd.com>
2023-03-01 11:42:31 +0530

    custom popup

---
+++ lib/widgets/popup_and_loaders/custom_popup.dart
@@ -0,0 +1,50 @@
+import 'package:driving_grade_book/main_common.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+customPopup(Widget child,
+    {EdgeInsetsGeometry? contentPadding,
+    bool isCloseIconVisible = false}) async {
+  await showDialog(
+      context: MyApp.navigatorKey.currentContext!,
+      useSafeArea: true,
+      barrierDismissible: true,
+      builder: (context) => AlertDialog(
+            insetPadding:
+                const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            titlePadding: const EdgeInsets.all(0),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            contentPadding: contentPadding ?? const EdgeInsets.all(0),
+            clipBehavior: Clip.antiAlias,
+            elevation: 4,
+            actionsPadding: const EdgeInsets.all(0),
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            content: SizedBox(
+              width: Get.width,
+              child: isCloseIconVisible
+                  ? SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                  constraints: const BoxConstraints(),
+                                  padding: const EdgeInsets.only(
+                                      top: 10, left: 10, bottom: 10, right: 4),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  icon: const Icon(Icons.clear)),
+                            ],
+                          ),
+                          child,
+                        ],
+                      ),
+                    )
+                  : child,
+            ),
+          ));
+}
